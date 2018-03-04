b83759106aa679fd5236f8e88d4e9f4c7cf69f60
hyunmoahn <mos_dev@naver.com>
2018-03-05 00:40:17 +0900

    webpack-dev-server 추가 후 개발시 환경설정

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,5 +2,13 @@ const webpackMerge = require('webpack-merge');
 const webpackBaseConfig = require('./webpack.basic.config');
 
 module.exports = webpackMerge(webpackBaseConfig, {
-  cache: true
+  cache: true,
+  devServer: {
+    contentBase: 'build',
+    disableHostCheck: true, // do not use production mode
+    historyApiFallback: true,
+    hot: true,
+    host: '0.0.0.0',
+    port: 3000
+  }
 })

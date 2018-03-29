2a8d0bea5fc3f046fe339d096ecf8038f950fec2
hyunmoahn <mos_dev@naver.com>
2018-03-29 15:03:31 +0900

    다국어 지원 선택을 바꾸어주는 LanguageSelect 컴포넌트 작성

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { hot } from 'react-hot-loader';
 import I18n from 'components/I18n';
+import LanguageSelect from 'components/LanguageSelect';
 
 const propTypes = {
   children: PropTypes.element.isRequired,
@@ -11,6 +12,7 @@ function App(props) {
   return (
     <div className="App">
       <I18n>
+        <LanguageSelect />
         {props.children}
       </I18n>
     </div>

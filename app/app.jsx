d52edf4de4557c8528e08b32f32a886632055fc5
hyunmoahn <mos_dev@naver.com>
2018-03-30 20:25:42 +0900

    I18n을 관리하기 위한 utils class 추가
    i18n에 대한 정보를 i18n class에서 관리하고
    데이터를 전달해주는 작업을 I18nWrapper에서 전해줍니다.

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { hot } from 'react-hot-loader';
-import I18n from 'components/I18n';
+import I18nWrapper from 'components/I18nWrapper';
 import LanguageSelect from 'components/LanguageSelect';
 
 const propTypes = {
@@ -11,10 +11,10 @@ const propTypes = {
 function App(props) {
   return (
     <div className="App">
-      <I18n>
+      <I18nWrapper>
         <LanguageSelect />
         {props.children}
-      </I18n>
+      </I18nWrapper>
     </div>
   );
 }
